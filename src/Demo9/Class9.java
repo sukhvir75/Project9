cfd2c3c47dd010ab16c3249882d61c5985e0132c
sukhvir75 <140761481+sukhvir75@users.noreply.github.com>
2023-07-31 22:53:16 -0400

    gscvxjashgks

---
+++ src/Demo9/Class9.java
@@ -0,0 +1,18 @@
+package Demo9;
+
+public class Class9 {
+	public static void main(String args []){
+		System.out.println("Hello..");
+		System.out.println("Hello..");
+
+   }
+	//add method
+	public static void add(int a,int b) {
+		System.out.println(a+b);
+	}
+	public static void dvide(int a,int b) {
+		
+		System.out.println(a/b);
+
+  }
+}
