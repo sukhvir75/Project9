33803ee67e6561890fb0749253e639dbadad23d5
sukhvir75 <140761481+sukhvir75@users.noreply.github.com>
2023-08-01 00:11:27 -0400

    gxvjshcbdhkasjcn

--- src/Day5/DemoDay5.java
+++ src/Day5/DemoDay5.java
@@ -4,5 +4,8 @@ public class DemoDay5 {
 	public static void main(String args []) {
 		System.out.println("Nice weather...");
 	}
+	public static void getInfo() {
+		System.out.println("nice weather...");
+	}
 
 }
---
+++ src/Demo7/DemoDay7.java
@@ -0,0 +1,10 @@
+package Demo7;
+
+public class DemoDay7 {
+	public static void main(String args []) {
+		System.out.println("Nice weather...");
+
+  }
+
+
+}
