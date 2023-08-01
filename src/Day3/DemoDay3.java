dbfb82c4b580776bbe043b5ed455d8eaf5504ae3
sukhvir75 <140761481+sukhvir75@users.noreply.github.com>
2023-07-31 23:10:24 -0400

    Update DemoDay3.java

--- src/Day3/DemoDay3.java
+++ src/Day3/DemoDay3.java
@@ -2,7 +2,8 @@ package Day3;
 
 public class DemoDay3 {
 	public static void main(String args []) {
-		
+		String country ="USA";
+		String state ="Va";
 		System.out.println("Sukhvir");
 		System.out.println("Sukhvir");
 		
